e2e1ea0dc97cafd37c66caf90ae1004d7ad301b9
Rômulo Ritter Contini <romulorc@gmail.com>
2020-05-01 14:53:23 -0300

    Corrige bug ao cadastrar usuario sem informação de artista

--- models/User.js
+++ models/User.js
@@ -1,19 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var artistSchema = new Schema({
+    name: { type: String, required: true },
+    genre: { type: String, required: true },
+    bio: String,
+    instagram: String,
+    youtube: String,
+    facebook: String,
+    spotify: String
+})
+
 var userSchema = new Schema({
     username: { type: String, required: true, unique: true, lowercase: true, index: true },
     createdAt: { type: Date, required: true, default: new Date() },
     mobile: { type: String, required: true },
-    artist: {
-        name: { type: String, required: true },
-        genre: { type: String, required: true },
-        bio: String,
-        instagram: String,
-        youtube: String,
-        facebook: String,
-        spotify: String
-    }
+    artist: artistSchema
 });
 
 userSchema.statics.findByUsername = function(username) {
